7626a1e404acaf28a81968404bd49e4866fa2075
zaeem0 <zaeem.q97@gmail.com>
2019-09-09 21:46:15 +0100

    delete logs

--- src/components/logs/LogItem.js
+++ src/components/logs/LogItem.js
@@ -1,26 +1,35 @@
 import React from "react";
 import Moment from "react-moment";
+import { connect } from "react-redux";
+import { deleteLog } from "../../actions/logActions";
 import PropTypes from "prop-types";
+import MaterializeJS from "materialize-css/dist/js/materialize.min.js";
 
-const LogItem = ({ log }) => {
+const LogItem = ({
+  log: { id, attention, message, tech, date },
+  deleteLog
+}) => {
+  const onDelete = () => {
+    deleteLog(id);
+    MaterializeJS.toast({ html: "Log Deleted" });
+  };
+
   return (
     <li className="collection-item">
       <div>
         <a
           href="#edit-log-modal"
-          className={`modal-trigger ${
-            log.attention ? "red-text" : "blue-text"
-          }`}
+          className={`modal-trigger ${attention ? "red-text" : "blue-text"}`}
         >
-          {log.message}
+          {message}
         </a>
         <br />
         <span className="grey-text">
-          <span className="black-text">ID #{log.id}</span> last updated by{" "}
-          <span className="black-text">{log.tech}</span> on{" "}
-          <Moment format="MMMM Do YYYY, h:mm:ss a">{log.date}</Moment>
+          <span className="black-text">ID #{id}</span> last updated by{" "}
+          <span className="black-text">{tech}</span> on{" "}
+          <Moment format="MMMM Do YYYY, h:mm:ss a">{date}</Moment>
         </span>
-        <a href="#!" className="secondary-content">
+        <a onClick={onDelete} href="#!" className="secondary-content">
           <i className="material-icons grey-text">delete</i>
         </a>
       </div>
@@ -29,7 +38,11 @@ const LogItem = ({ log }) => {
 };
 
 LogItem.propType = {
-  log: PropTypes.object.isRequired
+  log: PropTypes.object.isRequired,
+  deleteLog: PropTypes.func.isRequired
 };
 
-export default LogItem;
+export default connect(
+  null,
+  { deleteLog }
+)(LogItem);
